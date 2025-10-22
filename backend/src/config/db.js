9c38ae6531d5ab4381b8f916e5e866e7ccc2c8c1
Soss <l56eerl@gmail.com>
2025-10-22 06:17:52 +0300

    yml file update

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 
-dotenv.config()
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to MongoDB:");
+   const conn= await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB:", conn.connection.host);
   } catch (error) {
     console.error(error);
     process.exit(1);
