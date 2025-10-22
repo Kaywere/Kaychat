de14eb5d46f0e1c5535b267df3618345177a21bd
Soss <l56eerl@gmail.com>
2025-10-22 06:00:49 +0300

    Fixed DB connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,9 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-   const conn= await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to MongoDB:", conn.connection.host);
-    console.log(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB:");
   } catch (error) {
     console.error(error);
     process.exit(1);
