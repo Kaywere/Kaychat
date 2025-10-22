2b90d9f42ea06edf33ed06414dd4242f7fc3ff8f
Soss <l56eerl@gmail.com>
2025-10-22 05:57:40 +0300

    Fixed DB connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,6 +4,7 @@ const connectDB = async () => {
   try {
    const conn= await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB:", conn.connection.host);
+    console.log(process.env.MONGO_URI);
   } catch (error) {
     console.error(error);
     process.exit(1);
---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,23 @@
+import User from "../models/User.js";
+
+export const signup = async (req, res) => {
+  const { fullname, email, password } = req.body;
+
+  try {
+    if (!fullname || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 character" });
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (email == 0) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 character" });
+    }
+  } catch (error) {}
+};
